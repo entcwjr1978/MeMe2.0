dd66b17c2b8fd97aaf5aba43c5fbdc81684d52d3
Ed Williams <Ed.Williams@coxinc.com>
2016-11-13 22:55:52 -0500

    first commit

---
+++ MeMe/SentMemesCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SentMeMeCollectionViewController.swift
+//  MeMe
+//
+//  Created by Williams, Ed (CMG-Atlanta) on 11/13/16.
+//  Copyright © 2016 Edward Williams. All rights reserved.
+//
+
+import UIKit
+
+class SentMeMeCollectionViewController:UICollectionViewController {
+    
+}
---
+++ MeMe/SentMemesTableCollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SentMemesTableCollectionViewController.swift
+//  MeMe
+//
+//  Created by Williams, Ed (CMG-Atlanta) on 11/13/16.
+//  Copyright © 2016 Edward Williams. All rights reserved.
+//
+
+import Foundation
